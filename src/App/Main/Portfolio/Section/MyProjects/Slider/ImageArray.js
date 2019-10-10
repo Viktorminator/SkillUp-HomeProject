4a4eb283d2b46bdc5314ee863d3acfd3c740121b
Maksym Shamanskyi <shamanik43@gmail.com>
2019-10-10 18:34:24 +0300

    Add slider

---
+++ src/App/Main/Portfolio/Section/MyProjects/Slider/ImageArray.js
@@ -0,0 +1,29 @@
+
+
+const Images = [
+    {   
+        id: 1,
+        image:'/Images/collage-cite-1.png',
+        alt: 'img1'
+    },
+
+    {
+        id: 2,
+        image:"/Images/collage-cite-2.png",
+        alt: 'img2'
+    },
+
+    {
+        id: 3,
+        image:"/Images/collage-cite-3.png",
+        alt: 'img3'
+    },
+
+    {
+        id: 4,
+        image:"/Images/collage-cite-4.png",
+        alt: 'img4'
+    }
+]
+
+export default Images
--- src/App/Main/Portfolio/Section/MyProjects/Slider/Slider.js
+++ src/App/Main/Portfolio/Section/MyProjects/Slider/Slider.js
@@ -1,37 +1,80 @@
 import React, {Component}  from 'react'
-import {Carousel} from 'react-responsive-carousel'
+import ImageArray from './ImageArray'
 
-import "react-responsive-carousel/lib/styles/carousel.css"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import "react-responsive-carousel/lib/styles/main.css"
-import "react-responsive-carousel/lib/styles/main.min.css"
-
-import slide1 from './Images/collage-cite-1.png'
-import slide2 from './Images/collage-cite-2.png'
-import slide3 from './Images/collage-cite-3.png'
-import slide4 from './Images/collage-cite-4.png'
+import './Slider.css'
 
 class Slider extends Component{
+
+    state = {
+        position: {
+            left: '0px',
+        },
+        colorBtn: {
+            backgroundColor: 'rgb(102, 104, 105)'
+        }
+    }
+
+    newSlide(productId) {
+        if (productId == 1) {
+            this.setState (() => ({
+                position : {
+                    left: '0px'
+                }
+            }))
+        } else if (productId == 2) {
+            this.setState (() => ({
+                position : {
+                    left: '-1024px'
+                }
+            }))
+        } else if (productId == 3) {
+            this.setState (() => ({
+                position : {
+                    left: '-2048px'
+                }
+            }))
+        } else if (productId == 4) {
+            this.setState (() => ({
+                position : {
+                    left: '-3072px'
+                }
+            }))
+        }
+    }
+
     render() {
         return (
-            <Carousel>
-                <div>
-                    <img src={slide1} alt= "slide1" />
-                    <p className="legend">Legend 1</p>
+            <div className="slide-container">
+                <div className="image-container" style = {this.state.position} >
+                    {
+                        ImageArray.map(({
+                            image,
+                            alt,
+                            id
+                        }) => {
+                            return (
+                                <img src={image} alr={alt} key={id}/>
+                            )
+                        }
+                        )
+                    }
                 </div>
-                <div>
-                    <img src={slide2} alt= "slide2" />
-                    <p className="legend">Legend 2</p>
+                <div className="button-container">
+                    {
+                        ImageArray.map(({
+                            id
+                        })=>{
+                            return (
+                                <div
+                                    className="slider-button"
+                                    key={id}
+                                    onClick = {()=>this.newSlide(id)}
+                                />
+                            )
+                        })
+                    }
                 </div>
-                <div>
-                    <img src={slide3} alt= "slide3" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src={slide4} alt= "slide4" />
-                    <p className="legend">Legend 2</p>
-                </div>
-            </Carousel>
+            </div>
         )
     }
 }
