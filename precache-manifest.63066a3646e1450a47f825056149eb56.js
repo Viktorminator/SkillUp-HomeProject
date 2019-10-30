c540993c5c33f2171de8b7c13d054ee99b97f072
Maksym Shamanskyi <shamanik43@gmail.com>
2019-10-30 14:26:27 +0200

    Updates

---
+++ precache-manifest.63066a3646e1450a47f825056149eb56.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3a183de3856bfd776cb6c22a462314",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32ad76142434916dc792",
+    "url": "/static/css/2.d4c5f937.chunk.css"
+  },
+  {
+    "revision": "63b683925b9d0ef4f769",
+    "url": "/static/css/main.506c45b6.chunk.css"
+  },
+  {
+    "revision": "32ad76142434916dc792",
+    "url": "/static/js/2.91ff45a4.chunk.js"
+  },
+  {
+    "revision": "63b683925b9d0ef4f769",
+    "url": "/static/js/main.3b7a3e2f.chunk.js"
+  },
+  {
+    "revision": "24e0f57bde890e7ac330",
+    "url": "/static/js/runtime-main.2a9131c1.js"
+  },
+  {
+    "revision": "d740e153c4ddd60b71cbf2c0783024ae",
+    "url": "/static/media/_121.d740e153.jpg"
+  },
+  {
+    "revision": "200928488b4681276492d717d040b3ff",
+    "url": "/static/media/_122.20092848.jpg"
+  },
+  {
+    "revision": "6a4cf2a3f01a8639d3c415b51f636f4e",
+    "url": "/static/media/angular.6a4cf2a3.png"
+  },
+  {
+    "revision": "6afd37d8038531aa7f4837e386e44ba7",
+    "url": "/static/media/arrow.6afd37d8.png"
+  },
+  {
+    "revision": "9a99377a3dd80d2601b70dbecc93b9e7",
+    "url": "/static/media/chrome-footer.9a99377a.png"
+  },
+  {
+    "revision": "bee481814a101e00fe2f3a7c5c7063d1",
+    "url": "/static/media/chrome.bee48181.png"
+  },
+  {
+    "revision": "0e8a418787fe7f3450492b89ef605cae",
+    "url": "/static/media/css.0e8a4187.jpg"
+  },
+  {
+    "revision": "89034e466b83c3d147b4e8cc0196e8d0",
+    "url": "/static/media/html.89034e46.png"
+  },
+  {
+    "revision": "e725452994a2c3f54adef3bd6aea7585",
+    "url": "/static/media/iam.e7254529.jpg"
+  },
+  {
+    "revision": "0fe0c203e7b5312e6ca770e142221f67",
+    "url": "/static/media/instagram-footer.0fe0c203.png"
+  },
+  {
+    "revision": "30fa0886f8b6840a53f5deb91f9722f5",
+    "url": "/static/media/instagram.30fa0886.png"
+  },
+  {
+    "revision": "2507f585483783b90420bb1d784a6082",
+    "url": "/static/media/logo.2507f585.jpg"
+  },
+  {
+    "revision": "d93802d58aff61cd5facfe5106503257",
+    "url": "/static/media/pageUp.d93802d5.png"
+  },
+  {
+    "revision": "b646782bcd96dc460a99e8441bb49071",
+    "url": "/static/media/react.b646782b.png"
+  },
+  {
+    "revision": "220f37c5d39a292717efaf42d887c072",
+    "url": "/static/media/telegram.220f37c5.png"
+  },
+  {
+    "revision": "9951882c53e5b629cd9f9b998ebd5e64",
+    "url": "/static/media/twitter-footer.9951882c.png"
+  },
+  {
+    "revision": "0fd4ec61cbe213cbdfacb01a08400604",
+    "url": "/static/media/twitter.0fd4ec61.png"
+  }
+]);
